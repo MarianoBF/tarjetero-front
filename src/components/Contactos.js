3806b44f7fc3ab9251b0bbb72bfc674ced20fa74
MarianoBF <mariano.b.fernandez@gmail.com>
2021-04-08 17:49:36 -0300

    search & add compañía functionality

--- src/components/Contactos.js
+++ src/components/Contactos.js
@@ -4,6 +4,9 @@ import Table from "react-bootstrap/Table";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
+import ContactosBuscador from "./ContactosBuscador";
+import ContactosAgregar from "./ContactosAgregar";
+
 
 function Contactos() {
   const [lista, setLista] = useState([]);
@@ -20,8 +23,10 @@ function Contactos() {
         setLista(data.data);
         console.log(data.data);
       })
-      .catch(() => console.log("No se pudo traer la informcación"));
+      .catch(() => console.log("No se pudo traer la información"));
   }, []);
+
+
 
   const listaContactos = lista
     .sort((item1, item2) => (item1.nombre > item2.nombre ? 1 : -1))
@@ -39,45 +44,11 @@ function Contactos() {
       );
     });
 
-  const valorInicial = {
-    nombre: "",
-    apellido: "",
-    ciudad: "",
-    empresa: "",
-    cargo: "",
-    canalPreferido: "",
-    interes: "",
-  };
-
-  const [entrada, setEntrada] = useState(valorInicial);
-
-  const manejarInput = (event) => {
-    const { name, value } = event.target;
-    setEntrada({ ...entrada, [name]: value });
-  };
-
-  const guardarEmpresa = (e) => {
-    e.preventDefault();
-    const data = {
-      nombre: entrada.nombre,
-      apellido: entrada.apellido,
-      ciudad: entrada.ciudad,
-      empresa: entrada.empresa,
-      cargo: entrada.cargo,
-      canalPreferido: entrada.canalPreferido,
-      interes: entrada.interes,
-    };
-    servicioContacto
-      .sumar(data)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch(() => console.log("No se pudo agregar el contacto"));
-  };
-
   return (
     <div>
       <h3>Contactos</h3>
+
+      <ContactosBuscador lista={lista}/>
 
       <button onClick={cambiarModo}>
         {!modoAgregar ? "Agregar" : "Listar"}
@@ -100,74 +71,7 @@ function Contactos() {
             <tbody>{listaContactos ? listaContactos : "Problemas"}</tbody>
           </Table>
         </div>
-      ) : (
-        <div>
-          <h3>Agregar Empresa:</h3>
-          <Form onSubmit={guardarEmpresa}>
-            <Col md={6}>
-              <Form.Group>
-                <Form.Label>Nombre: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={entrada.nombre}
-                  onChange={manejarInput}
-                  name="nombre"
-                  required
-                ></Form.Control>
-                <Form.Label>Apellido: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={entrada.apellido}
-                  onChange={manejarInput}
-                  name="apellido"
-                  required
-                ></Form.Control>
-                <Form.Label>Ciudad: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={entrada.ciudad}
-                  onChange={manejarInput}
-                  name="ciudad"
-                  required
-                ></Form.Control>
-                <Form.Label>Empresa: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={entrada.empresa}
-                  onChange={manejarInput}
-                  name="empresa"
-                  required
-                ></Form.Control>
-                <Form.Label>Cargo: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={entrada.cargo}
-                  onChange={manejarInput}
-                  name="cargo"
-                  required
-                ></Form.Control>
-                <Form.Label>Canal Preferido: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={entrada.canalPreferido}
-                  onChange={manejarInput}
-                  name="canalPreferido"
-                  required
-                ></Form.Control>
-                <Form.Label>Interés: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={entrada.interes}
-                  onChange={manejarInput}
-                  name="interes"
-                  required
-                ></Form.Control>
-              </Form.Group>
-            </Col>
-            <Button type="submit">Guardar</Button>
-          </Form>
-        </div>
-      )}
+      ) : <ContactosAgregar />}
     </div>
   );
 }
