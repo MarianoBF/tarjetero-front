3299d249643061da54bcbc5220c1a38969e888e2
MarianoBF <mariano.b.fernandez@gmail.com>
2021-04-13 12:29:27 -0300

    edit/delete Usuarios, add in separate component

--- src/components/Usuarios.js
+++ src/components/Usuarios.js
@@ -1,16 +1,20 @@
 import servicioUsuario from "../services/Usuario_servicio";
 import {useState, useEffect} from "react";
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
 import Table from "react-bootstrap/Table";
 import jwt_decode from "jwt-decode";
+import borrar from "../media/borrar.svg";
+import editar from "../media/editar.svg";
+import UsuarioAgregar from "./UsuariosAgregar.js";
+
+
 
 function Usuarios() {
   const [modoRegistro, setModoRegistro] = useState(false);
 
-  const cambiarModo = () => {
-    setModoRegistro(!modoRegistro);
+  const manejarRegistrar = () => {
+    setModoRegistro(true);
+    setModoEditar(false);
   };
 
   const valorInicial = {
@@ -22,40 +26,27 @@ function Usuarios() {
     confirmarPassword: "",
   };
 
-  const [entrada, setEntrada] = useState(valorInicial);
-
-  const manejarInput = event => {
-    const {name, value} = event.target;
-    setEntrada({...entrada, [name]: value});
-  };
-
-  const manejarCancelar = () => {
-    setEntrada();
-    setModoRegistro(false);
-  };
-
-  const crearUsuario = e => {
-    e.preventDefault();
-    if (entrada.password !== entrada.confirmarPassword) {
-      return alert("Los password no coinciden!");
-    }
-    const data = {
-      nombre: entrada.nombre,
-      apellido: entrada.apellido,
-      email: entrada.email,
-      password: entrada.password,
-      perfil: entrada.perfil,
-    };
-    servicioUsuario
-      .sumar(data)
-      .then(response => {
-        console.log(response.data);
-        alert("Usuario creado con éxito!");
-        window.location.reload();
-      })
-      .catch(() => console.log("No se pudo agregar el usuario"));
-  };
   const [listadoUsuarios, setListadoUsuarios] = useState([]);
+
+  const manejarBorrar = (id) => {
+    servicioUsuario.borrar(id);
+    window.location.reload()
+  };
+
+  const [modoEditar, setModoEditar] = useState(false);
+
+  const [entradaEditar, setEntradaEditar] = useState();
+
+  const manejarEditar = item => {
+    setModoEditar(true);
+    setEntradaEditar(item);
+  };
+
+  const manejarCancelarEdicion = () => {
+    setModoEditar(false);
+    setModoRegistro(false);
+    setEntradaEditar("");
+  };
 
   const listadoDeUsuarios = listadoUsuarios
     .sort((item1, item2) => (item1.pais > item2.pais ? 1 : -1))
@@ -66,25 +57,20 @@ function Usuarios() {
           <td>{item.apellido}</td>
           <td>{item.email}</td>
           <td>{item.perfil}</td>
-          {/* <td>
+          <td>
             <div className="centrarContenidos"
               onClick={() =>
                 manejarEditar(
-                  item.nombre,
-                  item.pais,
-                  item.ciudad,
-                  item.direccion,
-                  item.email,
-                  item.telefono
+                  item
                 )
               }>
               <img className="icon" src={editar} alt="editar" />
             </div>
-          </td> */}
+          </td>
 
-          {/* <td>
-            <div className="centrarContenidos" onClick={() => manejarBorrar(item.nombre)}><img className="icon" src={borrar} alt="borrar" /></div>
-          </td> */}
+          <td>
+            <div className="centrarContenidos" onClick={() => manejarBorrar(item._id)}><img className="icon" src={borrar} alt="borrar" /></div>
+          </td>
         </tr>
       );
     });
@@ -116,11 +102,11 @@ function Usuarios() {
 
   return (
     <div className="centrarContenidos">
-      {modoAdmin && !modoRegistro && (
+      {modoAdmin && !modoRegistro && !modoEditar && (
         <div>
           <div className="tituloCompartido">
             <h3>Usuarios existentes</h3>
-            <Button onClick={cambiarModo}>Agregar un nuevo usuario</Button>
+            <Button onClick={manejarRegistrar}>Agregar un nuevo usuario</Button>
           </div>
           <Table>
             <thead className="fondoNaranja">
@@ -129,80 +115,31 @@ function Usuarios() {
                 <th>Apellido</th>
                 <th>Email</th>
                 <th>Perfil</th>
+                <th>Editar</th>
+                <th>Borrar</th>
+
               </tr>
             </thead>
             <tbody>{listadoDeUsuarios}</tbody>
           </Table>
         </div>
       )}
+
       {modoAdmin && modoRegistro ? (
-        <div>
-          <h3>Formulario de registro</h3>
-          <Form onSubmit={crearUsuario}>
-            <Col className="bloqueCentrado" md={6}>
-              <Form.Group>
-                <Form.Label>Nombre: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={entrada.nombre}
-                  onChange={manejarInput}
-                  name="nombre"
-                  required></Form.Control>
-                <Form.Label>Apellido: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={entrada.apellido}
-                  onChange={manejarInput}
-                  name="apellido"
-                  required></Form.Control>
-                <Form.Label>Email: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={entrada.email}
-                  onChange={manejarInput}
-                  name="email"
-                  required></Form.Control>
-                <Form.Label>Password: </Form.Label>
-                <Form.Control
-                  type="password"
-                  value={entrada.password}
-                  onChange={manejarInput}
-                  name="password"
-                  autoComplete="new-password"
-                  required></Form.Control>
-                <Form.Label>Confirmar Password: </Form.Label>
-                <Form.Control
-                  type="password"
-                  value={entrada.confirmarPassword}
-                  onChange={manejarInput}
-                  name="confirmarPassword"
-                  required></Form.Control>
-                <Form.Label>Perfil: </Form.Label>
-                <Form.Control
-                  as="select"
-                  value={entrada.perfil}
-                  onChange={manejarInput}
-                  name="perfil"
-                  required>
-                  <option></option>
-                  <option>Básico</option>
-                  <option>Admin</option>
-                </Form.Control>
-              </Form.Group>
-              <Button className="bloqueCentrado" type="submit" size="lg">
-                Crear Usuario
-              </Button>
-              <hr />
-              <Button
-                className="bloqueCentrado"
-                onClick={manejarCancelar}
-                variant="danger">
-                Cancelar
-              </Button>
-            </Col>
-          </Form>
-        </div>
+        <UsuarioAgregar
+          entrada={valorInicial}
+          editar={modoEditar}
+          cancelar={manejarCancelarEdicion}
+        />
       ) : null}
+      {modoAdmin && modoEditar ? (
+        <UsuarioAgregar
+          entrada={entradaEditar}
+          editar={modoEditar}
+          cancelar={manejarCancelarEdicion}
+        />
+      ) : null}
+
     </div>
   );
 }
--- src/services/Usuario_servicio.js
+++ src/services/Usuario_servicio.js
@@ -14,6 +14,22 @@ class servicioUsuario {
       return http.post("/login", data);
   }
 
+  encontrar(data) {
+    return http.put("/ubicacion", data);
+  }
+
+  actualizar(data) {
+    return http.put("/usuario/" + data);
+  }
+
+  borrar(data) {
+    return http.delete("/usuario/" + data);
+  }
+
+  borrarTodo() {
+    return http.delete("/usuarios");
+  }
+
 }
 
 export default new servicioUsuario();
